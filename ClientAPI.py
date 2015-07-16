e3f8cddd0a8fc891ec4ee03332bac4f8cabb2d3b
ahmed-youssef <ahmedalaa90@gmail.com>
2015-07-15 23:55:46 -0400

    Update ClientAPI.py

--- ClientAPI.py
+++ ClientAPI.py
@@ -1,12 +1,5 @@
 import xmlrpclib
 
-XmlString = open("test1.xml").read()
-
-yuns = YunServer.Check()
-
-for yun in yuns:
-	print yun
-
 s = xmlrpclib.ServerProxy('http://127.0.0.1:8000')
-
+XmlString = open("test1.xml").read()
 s.Create(XmlString, "192.168.54.14")
